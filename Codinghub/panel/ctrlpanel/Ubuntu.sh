190966527719f5a24388a3e5aded305673fb26d7
𝙏𝙝𝙚 𝘾𝙤𝙙𝙞𝙣𝙜 𝙝𝙪𝙗 <nn8109239@gmail.com>
2026-03-06 22:05:22 +0530

    Update Ubuntu.sh

--- Codinghub/panel/ctrlpanel/Ubuntu.sh
+++ Codinghub/panel/ctrlpanel/Ubuntu.sh
@@ -8,9 +8,6 @@ LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 
-# MariaDB repo setup script (Ubuntu 20.04 only!!!)
-curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash
-
 # Update repositories list
 apt update -y
 
